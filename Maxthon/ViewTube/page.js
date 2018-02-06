8be5c32fe76446e099eadf8f2d4b583ba957ace0
sebaro <esebaro@gmail.com>
2018-02-06 10:14:50 +0200

    Remove Old Extensions

--- Maxthon/ViewTube/page.js
+++
@@ -1,4 +0,0 @@
-javascript:
-var myRuntime = window.external.mxGetRuntime();
-var mxTabs = myRuntime.create('mx.browser.tabs');
-var TabNum = mxTabs.newTab({url:"http://isebaro.com/viewtube/",activate: true,position:"afterCurrrent"});
