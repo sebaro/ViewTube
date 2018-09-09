134608871a48da043ad45d32a4da57bf487b872d
sebaro <esebaro@gmail.com>
2018-09-09 13:48:27 +0300

    Update WebExtensions

--- Extensions/WebExtensions/ViewTube/viewtube.user.js
+++ Extensions/WebExtensions/ViewTube/viewtube.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name		ViewTube
-// @version		2018.08.08
+// @version		2018.09.09
 // @description		Watch videos from video sharing websites without Flash Player.
 // @author		sebaro
 // @namespace		http://sebaro.pro/viewtube
@@ -997,6 +997,8 @@ function ViewTube() {
       var ytSignFuncName, ytSignFuncBody, ytSwapFuncName, ytSwapFuncBody, ytFuncMatch;
       ytScriptSrc = ytScriptSrc.replace(/(\r\n|\n|\r)/gm, '');
       ytSignFuncName = ytScriptSrc.match(/"signature"\s*,\s*([^\)]*?)\(/);
+      //a.match(/https:\/\/yt.akamaized.net/)||d.set("alr","yes");c&&d.set(b,IK(c));return d};
+      if (!ytSignFuncName) ytSignFuncName = ytScriptSrc.match(/d.set\(b,(.*?)\(/);
       ytSignFuncName = (ytSignFuncName) ? ytSignFuncName[1] : null;
       if (ytSignFuncName) {
 	ytFuncMatch = ytSignFuncName.replace(/\$/, '\\$') + '\\s*=\\s*function\\s*' + '\\s*\\(\\w+\\)\\s*\\{(.*?)\\}';
@@ -1450,6 +1452,7 @@ function ViewTube() {
       var ytSignFuncName, ytSignFuncBody, ytSwapFuncName, ytSwapFuncBody, ytFuncMatch;
       ytScriptSrc = ytScriptSrc.replace(/(\r\n|\n|\r)/gm, '');
       ytSignFuncName = ytScriptSrc.match(/"signature"\s*,\s*([^\)]*?)\(/);
+      if (!ytSignFuncName) ytSignFuncName = ytScriptSrc.match(/d.set\(b,(.*?)\(/);
       ytSignFuncName = (ytSignFuncName) ? ytSignFuncName[1] : null;
       if (ytSignFuncName) {
 	ytFuncMatch = ytSignFuncName.replace(/\$/, '\\$') + '\\s*=\\s*function\\s*' + '\\s*\\(\\w+\\)\\s*\\{(.*?)\\}';
@@ -1882,7 +1885,7 @@ function ViewTube() {
 	  blockObject = dmPlayerWindow;
 	  dmSizes();
 	  styleMyElement(myPlayerWindow, {position: 'relative', width: dmPlayerWidth + 'px', height: dmPlayerHeight + 'px', backgroundColor: '#FFFFFF'});
-	  styleMyElement(dmPlayerWindow, {marginTop: '-20px'});
+	  styleMyElement(dmPlayerWindow, {marginTop: '-18px'});
 	  if (dmVideosReady) dmPlayer();
 	}
       }
@@ -1893,9 +1896,11 @@ function ViewTube() {
       }
       /* Hide Ads */
       var dmAdsTop = getMyElement('', 'div', 'class', 'AdTop__adTop___34Bs9', 0, false);
-      if (dmAdsTop) removeMyElement(dmAdsTop.parentNode, dmAdsTop);
-      var dmAdsBottom = getMyElement('', 'div', 'class', 'AdWatching__container___1jlOI', 0, false);
-      if (dmAdsBottom) removeMyElement(dmAdsBottom.parentNode, dmAdsBottom);
+      if (dmAdsTop && dmAdsTop.parentNode) removeMyElement(dmAdsTop.parentNode, dmAdsTop);
+      var dmAdsRightBottom = getMyElement('', 'div', 'class', 'AdWatching__container___1jlOI', 0, false);
+      if (dmAdsRightBottom && dmAdsRightBottom.parentNode) removeMyElement(dmAdsRightBottom.parentNode, dmAdsRightBottom);
+      var dmAdsRight = getMyElement('', 'div', 'class', 'AdWatchingRight__container___1jQ98', 0, false);
+      if (dmAdsRight && dmAdsRight.parentNode && dmAdsRight.parentNode.parentNode) removeMyElement(dmAdsRight.parentNode.parentNode, dmAdsRight.parentNode);
     }, 500);
 
     /* Create Player */
@@ -2762,7 +2767,7 @@ function ViewTube() {
       };
       feature['container'] = false;
       feature['widesize'] = false;
-      option['definitions'] = ['Full High Definition', 'High Definition', 'Standard Definition', 'Low Definition'];
+      option['definitions'] = ['Full High Definition', 'High Definition', 'Standard Definition', 'Low Definition', 'Very Low Definition'];
       option['containers'] = ['MP4'];
       createMyPlayer();
     }
@@ -2786,7 +2791,7 @@ function ViewTube() {
     /* Get Videos */
     var imdbVideoList = {};
     if (imdbVideosContent) {
-      var imdbVideoFormats = {'SD': 'Low Definition MP4', '480p': 'Standard Definition MP4', '720p': 'High Definition MP4', '1080p': 'Full High Definition MP4'};
+      var imdbVideoFormats = {'1080p': 'Full High Definition MP4', '720p': 'High Definition MP4', '480p': 'Standard Definition MP4', '360p': 'Low Definition MP4', 'SD': 'Low Definition MP4', '240p': 'Very Low Definition MP4'};
       var imdbVideoFound = false;
       var imdbVideoParser, imdbVideoParse, myVideoCode, imdbVideo;
       for (var imdbVideoCode in imdbVideoFormats) {
